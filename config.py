fce2c1fb59d8768c57270960f70e2782f968678f
jhanvii-rawat <jhanvii@DESKTOP-7F411H8.localdomain>
2024-06-17 21:03:14 +0530

    nav bar update

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 from dotenv import load_dotenv
-import os
-from app import app
+#import os
+
+from flask import Flask
+
+app = Flask(__name__)
 
 
 load_dotenv()  ##to load information from .env 
 
-app.config['SECRET_KEY']= os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///db.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
-
+#app.config['SECRET_KEY']= os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI']=  "sqlite:///db.sqlite3"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 
 
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from app import app
+from config import app
 from flask_sqlalchemy import SQLAlchemy
 
 db= SQLAlchemy(app)
